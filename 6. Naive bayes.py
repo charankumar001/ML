185ab8e1fe9863a85d3dde9a8accbd201fd19709
charankumar001 <charankumar082007.v@gmail.com>
2026-01-22 12:37:14 +0530

    Created using Colab

--- 6. Naive bayes.py
+++ 6. Naive bayes.py
@@ -4,7 +4,7 @@
   "metadata": {
     "colab": {
       "provenance": [],
-      "authorship_tag": "ABX9TyOxzUl6RNteg9tzyuoQT/tZ",
+      "authorship_tag": "ABX9TyMRq6jm2cmLeaIIuh+hteg7",
       "include_colab_link": true
     },
     "kernelspec": {
@@ -28,30 +28,14 @@
     },
     {
       "cell_type": "code",
-      "source": [
-        "from sklearn.datasets import load_iris\n",
-        "from sklearn.model_selection import train_test_split\n",
-        "from sklearn.naive_bayes import GaussianNB\n",
-        "from sklearn.metrics import confusion_matrix, accuracy_score\n",
-        "X, y = load_iris(return_X_y=True)\n",
-        "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)\n",
-        "naive_bayes_classifier = GaussianNB()\n",
-        "naive_bayes_classifier.fit(X_train, y_train)\n",
-        "y_pred = naive_bayes_classifier.predict(X_test)\n",
-        "conf_matrix = confusion_matrix(y_test, y_pred)\n",
-        "print(\"Confusion Matrix:\")\n",
-        "print(conf_matrix)\n",
-        "accuracy = accuracy_score(y_test, y_pred) * 100\n",
-        "print(f\"Accuracy: {accuracy:.2f}%\")\n"
-      ],
+      "execution_count": 1,
       "metadata": {
         "colab": {
           "base_uri": "https://localhost:8080/"
         },
-        "id": "34BmCl1yFnai",
-        "outputId": "f4883e00-ab3a-46d5-e017-e24767e499d8"
+        "id": "MUReaYCqgygi",
+        "outputId": "317dab4c-41c0-4ef8-b46a-f51753f02d50"
       },
-      "execution_count": 2,
       "outputs": [
         {
           "output_type": "stream",
@@ -64,6 +48,29 @@
             "Accuracy: 100.00%\n"
           ]
         }
+      ],
+      "source": [
+        "from sklearn.datasets import load_iris\n",
+        "from sklearn.model_selection import train_test_split\n",
+        "from sklearn.naive_bayes import GaussianNB\n",
+        "from sklearn.metrics import confusion_matrix, accuracy_score\n",
+        "\n",
+        "X, y = load_iris(return_X_y=True)\n",
+        "\n",
+        "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)\n",
+        "\n",
+        "naive_bayes_classifier = GaussianNB()\n",
+        "\n",
+        "naive_bayes_classifier.fit(X_train, y_train)\n",
+        "\n",
+        "y_pred = naive_bayes_classifier.predict(X_test)\n",
+        "\n",
+        "conf_matrix = confusion_matrix(y_test, y_pred)\n",
+        "print(\"Confusion Matrix:\")\n",
+        "print(conf_matrix)\n",
+        "\n",
+        "accuracy = accuracy_score(y_test, y_pred) * 100\n",
+        "print(f\"Accuracy: {accuracy:.2f}%\")"
       ]
     }
   ]
